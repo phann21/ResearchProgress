eb369da3a36496d3770ff81fe2cf1a5900481733
Philip Han <prh5154@psu.edu>
2025-03-05 16:15:30 -0500

    Commenting code and found a bug

--- redo.c
+++ redo.c
@@ -4,6 +4,7 @@
 
 #define DIMENSION 3
 
+//3x3 -> 1x1
 double matrix_determinant_3x3(double input_matrix[][DIMENSION]){
     double first_line = input_matrix[0][0] * (input_matrix[1][1] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][1]); 
     double second_line = input_matrix[0][1] * (input_matrix[1][0] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][0]);
@@ -12,6 +13,7 @@ double matrix_determinant_3x3(double input_matrix[][DIMENSION]){
     return first_line - second_line + third_line;
 }
 
+//3x3 -> 3x3
 double* matrix_invert(double input_matrix[][DIMENSION]){
     double output_matrix[DIMENSION][DIMENSION] = {  {input_matrix[1][1] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][1], (-1) * (input_matrix[1][0] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][0]), input_matrix[1][0] * input_matrix[2][1] - input_matrix[1][1] * input_matrix[2][0]},
                                                     {(-1) * (input_matrix[0][1] * input_matrix[2][2] - input_matrix[0][2] * input_matrix[2][1]), (input_matrix[0][0] * input_matrix[2][2] - input_matrix[0][2] * input_matrix[2][0]), (-1) * (input_matrix[0][0] * input_matrix[2][1] - input_matrix[0][1] * input_matrix[2][0])},
@@ -26,6 +28,8 @@ double* matrix_invert(double input_matrix[][DIMENSION]){
     return output_matrix;
 }
 
+//3x1 * 1x3 -> 3x3
+//INCORRECT
 double* matrix_multiply_3x1_1x3(double matrix_1[DIMENSION], double matrix_2[DIMENSION]){
     double result[DIMENSION] = {0,0,0};
     for (int i=0; i<DIMENSION; i++){
@@ -34,6 +38,7 @@ double* matrix_multiply_3x1_1x3(double matrix_1[DIMENSION], double matrix_2[DIME
     return result;
 }
 
+//1x3 * 3x1 -> 1x1
 double matrix_multiply_1x3_3x1(double matrix_1[DIMENSION], double matrix_2[DIMENSION]){
     double sum = 0;
     for (int i=0; i<DIMENSION; i++){
@@ -42,6 +47,7 @@ double matrix_multiply_1x3_3x1(double matrix_1[DIMENSION], double matrix_2[DIMEN
     return sum;
 }
 
+//3x1 * 1x1 -> 3x1
 double* matrix_scalar_multiply_3x1(double scalar, double matrix_1[DIMENSION]){
     double result[DIMENSION] = {0,0,0};
     for (int i=0; i<DIMENSION; i++){
@@ -50,6 +56,7 @@ double* matrix_scalar_multiply_3x1(double scalar, double matrix_1[DIMENSION]){
     return result;
 }
 
+//3x1 - 3x1 -> 3x1
 double* matrix_subtraction(double matrix_1[DIMENSION], double matrix_2[DIMENSION]){
     double result[DIMENSION] = {0,0,0};
     for (int i=0; i<DIMENSION; i++){
@@ -58,6 +65,7 @@ double* matrix_subtraction(double matrix_1[DIMENSION], double matrix_2[DIMENSION
     return result;
 }
 
+//3x1 + 3x1 -> 3x1
 double* matrix_addition(double matrix_1[DIMENSION], double matrix_2[DIMENSION]){
     double result[DIMENSION] = {0,0,0};
     for (int i=0; i<DIMENSION; i++){
@@ -70,6 +78,7 @@ double solve_K(double x, double y, double z){
     double K = pow(x,(double)2) + pow(y,(double)2) + pow(z,(double)2);
 }
 
+//3x3
 double* solve_A(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4){
     double A[DIMENSION][DIMENSION] = {{x2-x1, x3-x1, x4-x1},
                                     {y2-y1, y3-y1, y4-y1},
@@ -77,6 +86,7 @@ double* solve_A(double x1, double x2, double x3, double x4, double y1, double y2
     return A;
 }
 
+//3x1
 double* solve_b(double K1, double K2, double K3, double K4){
     double b[DIMENSION] = {0.5*(K2 - K1), 0.5*(K3 - K1), 0.5*(K4 - K1)};
     return b;
@@ -86,15 +96,20 @@ double create_ri_1(double ti, double t1){
     return ti - t1;
 }
 
+//3x1
 double* solve_d(double r2_1, double r3_1, double r4_1){
     double d[DIMENSION] = {r2_1, r3_1, r4_1};
     return d;
 }
 
+//3x1
 double* solve_e(double r2_1, double r3_1, double r4_1){
     double e[DIMENSION] = {pow(r2_1, (double)2), pow(r3_1, (double)2), pow(r4_1, (double)2)};
     return e;
 }
 
+// ar1^2 + br1 + c = 0
+ 
 int main() {
+    
 }
