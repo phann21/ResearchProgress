71fa4764d6771c13b9aeff6e4056c3fc199fe4c7
Philip Han <prh5154@psu.edu>
2025-03-20 00:16:25 -0400

    IT BUILDS!

--- newxc32_newfile.c
+++ newxc32_newfile.c
@@ -1,218 +1,218 @@
-/* 
- * Author: Philip Han
- */
+// /* 
+//  * Author: Philip Han
+//  */
 
-#include "CPUConfig.h"
-#include <xc.h>
-#include <sys/attribs.h>
-#include "my_delays.h"
+// //#include "CPUConfig.h"
+// //#include <xc.h>
+// //#include <sys/attribs.h>
+// //#include "my_delays.h"
 
 
-//MINI BOARD BUILTIN LEDs
-#define DATA_LED_IO TRISEbits.TRISE0=0
-#define STAT_LED_IO TRISEbits.TRISE1=0
-#define DATA_LED(bit) LATEbits.LATE0=bit 
-#define STAT_LED(bit) LATEbits.LATE1=bit 
-#define DIMENSION 3
-#define C 1480 // m/s^2
+// //MINI BOARD BUILTIN LEDs
+// #define DATA_LED_IO TRISEbits.TRISE0=0
+// #define STAT_LED_IO TRISEbits.TRISE1=0
+// #define DATA_LED(bit) LATEbits.LATE0=bit 
+// #define STAT_LED(bit) LATEbits.LATE1=bit 
+// #define DIMENSION 3
+// #define C 1480 // m/s^2
 
-//Inputs
-double t_diff[4] = {0,0,0,0};
-double t1;
-double t2;
-double t3;
-double t4;
-double input_matrix[DIMENSION][DIMENSION] = {{0,0,0},{0,0,0},{0,0,0}};
-double r_matrix[DIMENSION] = {0,0,0};
-double r1;
+// //Inputs
+// double t_diff[4] = {0,0,0,0};
+// double t1;
+// double t2;
+// double t3;
+// double t4;
+// double input_matrix[DIMENSION][DIMENSION] = {{0,0,0},{0,0,0},{0,0,0}};
+// double r_matrix[DIMENSION] = {0,0,0};
+// double r1;
 
-//Outputs
-double output_matrix[DIMENSION];
+// //Outputs
+// double output_matrix[DIMENSION];
 
-double ri_1_calculation(double ti, double t1){
-    return (double)(C * (ti - t1));
-}
+// double ri_1_calculation(double ti, double t1){
+//     return (double)(C * (ti - t1));
+// }
 
-double k_generation(double xi, double yi, double zi){
-    return xi*xi + yi*yi + zi*zi;
-}
+// double k_generation(double xi, double yi, double zi){
+//     return xi*xi + yi*yi + zi*zi;
+// }
 
-//add matrices
-double* matrix_add(double input1[DIMENSION], double input2[DIMENSION]){
-    double output[DIMENSION];
-    for (int i=0; i<DIMENSION; i++){
-        output[i] = input1[i] + input2[i];
-    }
-    return output;
-}
+// //add matrices
+// double* matrix_add(double input1[DIMENSION], double input2[DIMENSION]){
+//     double output[DIMENSION];
+//     for (int i=0; i<DIMENSION; i++){
+//         output[i] = input1[i] + input2[i];
+//     }
+//     return output;
+// }
 
-double* matrix_scalar_multiply(double input[DIMENSION], double scalar){
-    double output[DIMENSION];
-    for (int i=0; i<DIMENSION; i++){
-        output[i] = input[i] * scalar;
-    }
-    return output;
-}
+// double* matrix_scalar_multiply(double input[DIMENSION], double scalar){
+//     double output[DIMENSION];
+//     for (int i=0; i<DIMENSION; i++){
+//         output[i] = input[i] * scalar;
+//     }
+//     return output;
+// }
 
-double* matrix_multiply(double input1[][DIMENSION], double input2[DIMENSION]){
-    double output[DIMENSION];
-    for (int i=0; i<DIMENSION; i++){
-        output[i] = 0;
-        for (int j=0; j<DIMENSION; j++){
-            output[i] += input1[i][j] * input2[j];
-        }
-    }
-    return output;
-}
+// double* matrix_multiply(double input1[][DIMENSION], double input2[DIMENSION]){
+//     double output[DIMENSION];
+//     for (int i=0; i<DIMENSION; i++){
+//         output[i] = 0;
+//         for (int j=0; j<DIMENSION; j++){
+//             output[i] += input1[i][j] * input2[j];
+//         }
+//     }
+//     return output;
+// }
 
-double* matrix_transpose(double input[][DIMENSION]){
-    double output[DIMENSION][DIMENSION];
-    for (int i=0; i<DIMENSION; i++){
-        for (int j=0; j<DIMENSION; j++){
-            output[i][j] = input[j][i];
-        }
-    }
-    return output;
-}
+// double* matrix_transpose(double input[][DIMENSION]){
+//     double output[DIMENSION][DIMENSION];
+//     for (int i=0; i<DIMENSION; i++){
+//         for (int j=0; j<DIMENSION; j++){
+//             output[i][j] = input[j][i];
+//         }
+//     }
+//     return output;
+// }
 
-double* populate_matrix1(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4){
-    double x2_1 = x2 - x1;
-    double x3_1 = x3 - x1;
-    double x4_1 = x4 - x1;
-    double y2_1 = y2 - y1;
-    double y3_1 = y3 - y1;
-    double y4_1 = y4 - y1;
-    double z2_1 = z2 - z1;
-    double z3_1 = z3 - z1;
-    double z4_1 = z4 - z1;
-    double matrix1[DIMENSION][DIMENSION] = {{-1 * x2_1, -1 * x3_1, -1 * x4_1}, {-1 * y2_1, -1 * y3_1, -1 * y4_1}, {-1 * z2_1, -1 * z3_1, -1 * z4_1}};
-    return matrix1;
-}
+// double* populate_matrix1(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4){
+//     double x2_1 = x2 - x1;
+//     double x3_1 = x3 - x1;
+//     double x4_1 = x4 - x1;
+//     double y2_1 = y2 - y1;
+//     double y3_1 = y3 - y1;
+//     double y4_1 = y4 - y1;
+//     double z2_1 = z2 - z1;
+//     double z3_1 = z3 - z1;
+//     double z4_1 = z4 - z1;
+//     double matrix1[DIMENSION][DIMENSION] = {{-1 * x2_1, -1 * x3_1, -1 * x4_1}, {-1 * y2_1, -1 * y3_1, -1 * y4_1}, {-1 * z2_1, -1 * z3_1, -1 * z4_1}};
+//     return matrix1;
+// }
 
-double* populate_matrix3(double r2_1, double r3_1, double r4_1, double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4){
-    double K1 = k_generation(x1, y1, z1);
-    double K2 = k_generation(x2, y2, z2);
-    double K3 = k_generation(x3, y3, z3);
-    double K4 = k_generation(x4, y4, z4);
-    double matrix3[DIMENSION];
+// double* populate_matrix3(double r2_1, double r3_1, double r4_1, double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4){
+//     double K1 = k_generation(x1, y1, z1);
+//     double K2 = k_generation(x2, y2, z2);
+//     double K3 = k_generation(x3, y3, z3);
+//     double K4 = k_generation(x4, y4, z4);
+//     double matrix3[DIMENSION];
     
-    matrix3[0] = r2_1 * r2_1 - K2 + K1;
-    matrix3[1] = r3_1 * r3_1 - K3 + K1;
-    matrix3[2] = r4_1 * r4_1 - K4 + K1;
-    return matrix3;
-}
+//     matrix3[0] = r2_1 * r2_1 - K2 + K1;
+//     matrix3[1] = r3_1 * r3_1 - K3 + K1;
+//     matrix3[2] = r4_1 * r4_1 - K4 + K1;
+//     return matrix3;
+// }
 
-double calculate_r1(double matrixA[][DIMENSION], double matrixB[DIMENSION], double matrixC[][DIMENSION]){
-    double a = 1;
-    double b = matrix_scalar_multiply(matrixB, -2);
-}
+// double calculate_r1(double matrixA[][DIMENSION], double matrixB[DIMENSION], double matrixC[][DIMENSION]){
+//     double a = 1;
+//     double b = matrix_scalar_multiply(matrixB, -2);
+// }
 
-///NOT DONE
-void calculate(double matrix2[DIMENSION], double r1, double t1, double t2, double t3, double t4, double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4){
-    double output[DIMENSION];
-    double matrix1[DIMENSION][DIMENSION] = populate_matrix1(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4);
+// ///NOT DONE
+// void calculate(double matrix2[DIMENSION], double r1, double t1, double t2, double t3, double t4, double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4){
+//     double output[DIMENSION];
+//     double matrix1[DIMENSION][DIMENSION] = populate_matrix1(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4);
     
-    double r2_1 = ri_1_calculation(t2, t1);
-    double r3_1 = ri_1_calculation(t3, t1);
-    double r4_1 = ri_1_calculation(t4, t1);
+//     double r2_1 = ri_1_calculation(t2, t1);
+//     double r3_1 = ri_1_calculation(t3, t1);
+//     double r4_1 = ri_1_calculation(t4, t1);
     
-    matrix2 = {r2_1, r3_1, r4_1};
-    matrix2 = matrix_scalar_multiply(matrix2, r1);
+//     matrix2 = {r2_1, r3_1, r4_1};
+//     matrix2 = matrix_scalar_multiply(matrix2, r1);
     
-    double matrix3[DIMENSION] = populate_matrix3(r2_1, r3_1, r4_1, x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4);
-    matrix3 = matrix_scalar_multiply(matrix3, 0.5);
+//     double matrix3[DIMENSION] = populate_matrix3(r2_1, r3_1, r4_1, x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4);
+//     matrix3 = matrix_scalar_multiply(matrix3, 0.5);
     
-    double combined[DIMENSION];
-    combined = matrix_add(matrix2, matrix3);
+//     double combined[DIMENSION];
+//     combined = matrix_add(matrix2, matrix3);
     
-    double output = matrix_multiply(matrix1, combined);
-    return output;
-}
+//     double output = matrix_multiply(matrix1, combined);
+//     return output;
+// }
 
-//https://www.aidanmocke.com/blog/2018/04/03/config/
-void set_performance_mode()
-{   
+// //https://www.aidanmocke.com/blog/2018/04/03/config/
+// void set_performance_mode()
+// {   
     
-    unsigned int cp0;
+//     unsigned int cp0;
 
-    // Unlock Sequence
-    asm volatile("di"); // Disable all interrupts
-    SYSKEY = 0x00000000;
-    SYSKEY = 0xAA996655;
-    SYSKEY = 0x556699AA;  
+//     // Unlock Sequence
+//     asm volatile("di"); // Disable all interrupts
+//     SYSKEY = 0x00000000;
+//     SYSKEY = 0xAA996655;
+//     SYSKEY = 0x556699AA;  
 
-    // PB1DIV
-    // Peripheral Bus 1 cannot be turned off, so there's no need to turn it on
-    PB1DIVbits.PBDIV = 1; // Peripheral Bus 1 Clock Divisor Control (PBCLK1 is SYSCLK divided by 2)
+//     // PB1DIV
+//     // Peripheral Bus 1 cannot be turned off, so there's no need to turn it on
+//     PB1DIVbits.PBDIV = 1; // Peripheral Bus 1 Clock Divisor Control (PBCLK1 is SYSCLK divided by 2)
 
-    // PB2DIV
-    PB2DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
-    PB2DIVbits.PBDIV = 1; // Peripheral Bus 2 Clock Divisor Control (PBCLK2 is SYSCLK divided by 2)
+//     // PB2DIV
+//     PB2DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
+//     PB2DIVbits.PBDIV = 1; // Peripheral Bus 2 Clock Divisor Control (PBCLK2 is SYSCLK divided by 2)
 
-    // PB3DIV
-    PB3DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
-    PB3DIVbits.PBDIV = 1; // Peripheral Bus 3 Clock Divisor Control (PBCLK3 is SYSCLK divided by 2)
+//     // PB3DIV
+//     PB3DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
+//     PB3DIVbits.PBDIV = 1; // Peripheral Bus 3 Clock Divisor Control (PBCLK3 is SYSCLK divided by 2)
 
-    // PB4DIV
-    PB4DIVbits.ON = 1; // Peripheral Bus 4 Output Clock Enable (Output clock is enabled)
-    while (!PB4DIVbits.PBDIVRDY); // Wait until it is ready to write to
-    PB4DIVbits.PBDIV = 0; // Peripheral Bus 4 Clock Divisor Control (PBCLK4 is SYSCLK divided by 1)
+//     // PB4DIV
+//     PB4DIVbits.ON = 1; // Peripheral Bus 4 Output Clock Enable (Output clock is enabled)
+//     while (!PB4DIVbits.PBDIVRDY); // Wait until it is ready to write to
+//     PB4DIVbits.PBDIV = 0; // Peripheral Bus 4 Clock Divisor Control (PBCLK4 is SYSCLK divided by 1)
 
-    // PB5DIV
-    PB5DIVbits.ON = 1; // Peripheral Bus 5 Output Clock Enable (Output clock is enabled)
-    PB5DIVbits.PBDIV = 1; // Peripheral Bus 5 Clock Divisor Control (PBCLK5 is SYSCLK divided by 2)
+//     // PB5DIV
+//     PB5DIVbits.ON = 1; // Peripheral Bus 5 Output Clock Enable (Output clock is enabled)
+//     PB5DIVbits.PBDIV = 1; // Peripheral Bus 5 Clock Divisor Control (PBCLK5 is SYSCLK divided by 2)
 
-    // PB7DIV
-    PB7DIVbits.ON = 1; // Peripheral Bus 7 Output Clock Enable (Output clock is enabled)
-    PB7DIVbits.PBDIV = 0; // Peripheral Bus 7 Clock Divisor Control (PBCLK7 is SYSCLK divided by 1)
+//     // PB7DIV
+//     PB7DIVbits.ON = 1; // Peripheral Bus 7 Output Clock Enable (Output clock is enabled)
+//     PB7DIVbits.PBDIV = 0; // Peripheral Bus 7 Clock Divisor Control (PBCLK7 is SYSCLK divided by 1)
 
-    // PB8DIV
-    PB8DIVbits.ON = 1; // Peripheral Bus 8 Output Clock Enable (Output clock is enabled)
-    PB8DIVbits.PBDIV = 1; // Peripheral Bus 8 Clock Divisor Control (PBCLK8 is SYSCLK divided by 2)
+//     // PB8DIV
+//     PB8DIVbits.ON = 1; // Peripheral Bus 8 Output Clock Enable (Output clock is enabled)
+//     PB8DIVbits.PBDIV = 1; // Peripheral Bus 8 Clock Divisor Control (PBCLK8 is SYSCLK divided by 2)
 
-    // PRECON - Set up prefetch
-    PRECONbits.PFMSECEN = 0; // Flash SEC Interrupt Enable (Do not generate an interrupt when the PFMSEC bit is set)
-    PRECONbits.PREFEN = 3; // Predictive Prefetch Enable (Enable predictive prefetch for any address)
-    PRECONbits.PFMWS = 3; // PFM Access Time Defined in Terms of SYSCLK Wait States (Two wait states)
+//     // PRECON - Set up prefetch
+//     PRECONbits.PFMSECEN = 0; // Flash SEC Interrupt Enable (Do not generate an interrupt when the PFMSEC bit is set)
+//     PRECONbits.PREFEN = 3; // Predictive Prefetch Enable (Enable predictive prefetch for any address)
+//     PRECONbits.PFMWS = 3; // PFM Access Time Defined in Terms of SYSCLK Wait States (Two wait states)
     
-    //INTCONbits.MVEC = 1;
-    // Set up caching
-    cp0 = _mfc0(16, 0);
-    cp0 &= ~0x07;
-    cp0 |= 0b011; // K0 = Cacheable, non-coherent, write-back, write allocate
-    _mtc0(16, 0, cp0);  
+//     //INTCONbits.MVEC = 1;
+//     // Set up caching
+//     cp0 = _mfc0(16, 0);
+//     cp0 &= ~0x07;
+//     cp0 |= 0b011; // K0 = Cacheable, non-coherent, write-back, write allocate
+//     _mtc0(16, 0, cp0);  
 
-    // Lock Sequence
-    SYSKEY = 0x33333333;
-    asm volatile("ei"); // Enable all interrupts
-}
+//     // Lock Sequence
+//     SYSKEY = 0x33333333;
+//     asm volatile("ei"); // Enable all interrupts
+// }
 
 
-void init(void)
-{ 
-    set_performance_mode();
+// void init(void)
+// { 
+//     set_performance_mode();
 
-    // Set all of PORT pins to digital
-    ANSELB = 0;
-    ANSELE = 0;
-    ANSELG = 0;
+//     // Set all of PORT pins to digital
+//     ANSELB = 0;
+//     ANSELE = 0;
+//     ANSELG = 0;
     
-    DATA_LED_IO;
-    STAT_LED_IO;
-}
+//     DATA_LED_IO;
+//     STAT_LED_IO;
+// }
 
-void main(void) 
-{
-    init();
+// void main(void) 
+// {
+//     init();
    
-    while(1)
-    {
-       DATA_LED(1); STAT_LED(0); 
-       delay_ms(500);
-       DATA_LED(0); STAT_LED(1); 
-       delay_ms(500);
-    }
-}
+//     while(1)
+//     {
+//        DATA_LED(1); STAT_LED(0); 
+//        delay_ms(500);
+//        DATA_LED(0); STAT_LED(1); 
+//        delay_ms(500);
+//     }
+// }
 
 
 
--- redo.c
+++ redo.c
@@ -14,10 +14,16 @@ double matrix_determinant_3x3(double input_matrix[][DIMENSION]){
 }
 
 //3x3 -> 3x3
-double* matrix_invert(double input_matrix[][DIMENSION]){
-    double output_matrix[DIMENSION][DIMENSION] = {  {input_matrix[1][1] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][1], (-1) * (input_matrix[1][0] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][0]), input_matrix[1][0] * input_matrix[2][1] - input_matrix[1][1] * input_matrix[2][0]},
-                                                    {(-1) * (input_matrix[0][1] * input_matrix[2][2] - input_matrix[0][2] * input_matrix[2][1]), (input_matrix[0][0] * input_matrix[2][2] - input_matrix[0][2] * input_matrix[2][0]), (-1) * (input_matrix[0][0] * input_matrix[2][1] - input_matrix[0][1] * input_matrix[2][0])},
-                                                    {input_matrix[0][1] * input_matrix[1][2] - input_matrix[0][2] * input_matrix[1][1], (-1) * (input_matrix[0][0] * input_matrix[1][2] - input_matrix[0][2] * input_matrix[1][0]), input_matrix[0][0] * input_matrix[1][1] - input_matrix[0][1] * input_matrix[1][0]}};
+void matrix_invert(double input_matrix[][DIMENSION], double output_matrix[][DIMENSION]){
+    output_matrix[0][0] = input_matrix[1][1] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][1];
+    output_matrix[0][1] = (-1) * (input_matrix[1][0] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][0]);
+    output_matrix[0][2] = input_matrix[1][0] * input_matrix[2][1] - input_matrix[1][1] * input_matrix[2][0];
+    output_matrix[1][0] = (-1) * (input_matrix[0][1] * input_matrix[2][2] - input_matrix[0][2] * input_matrix[2][1]);
+    output_matrix[1][1] = (input_matrix[0][0] * input_matrix[2][2] - input_matrix[0][2] * input_matrix[2][0]);
+    output_matrix[1][2] = (-1) * (input_matrix[0][0] * input_matrix[2][1] - input_matrix[0][1] * input_matrix[2][0]);
+    output_matrix[2][0] = input_matrix[0][1] * input_matrix[1][2] - input_matrix[0][2] * input_matrix[1][1];
+    output_matrix[2][1] = (-1) * (input_matrix[0][0] * input_matrix[1][2] - input_matrix[0][2] * input_matrix[1][0]);
+    output_matrix[2][2] = input_matrix[0][0] * input_matrix[1][1] - input_matrix[0][1] * input_matrix[1][0];
     
     double determinant = matrix_determinant_3x3(input_matrix);
     for (int i = 0; i < DIMENSION; i++){
@@ -25,29 +31,36 @@ double* matrix_invert(double input_matrix[][DIMENSION]){
             output_matrix[i][j] = output_matrix[i][j] / determinant;
         }
     }
-    return output_matrix;
 }
 
 //3x1 * 1x3 -> 3x3
-double* matrix_multiply_3x1_1x3(double matrix_1[DIMENSION], double matrix_2[DIMENSION]){
-    double result[DIMENSION][DIMENSION] = {{0,0,0},{0,0,0},{0,0,0}};
+void matrix_multiply_3x1_1x3(double matrix_1[DIMENSION], double matrix_2[DIMENSION], double result[][DIMENSION]){
+    result[0][0] = 0;
+    result[0][1] = 0;
+    result[0][2] = 0;
+    result[1][0] = 0;
+    result[1][1] = 0;
+    result[1][2] = 0;
+    result[2][0] = 0;
+    result[2][1] = 0;
+    result[2][2] = 0;
     for (int i=0; i<DIMENSION; i++){
         for (int j=0; j<DIMENSION; j++){
             result[i][j] = matrix_1[i] * matrix_2[j];
         }
     }
-    return result;
 }
 
 //3x3 * 3x1 -> 3x1
-double* matrix_multiply_3x3_3x1(double matrix_1[DIMENSION][DIMENSION], double matrix_2[DIMENSION]){
-    double result[DIMENSION] = {0,0,0};
+void matrix_multiply_3x3_3x1(double matrix_1[DIMENSION][DIMENSION], double matrix_2[DIMENSION], double result[DIMENSION]){
+    result[0] = 0;
+    result[1] = 0;
+    result[2] = 0;
     for (int i=0; i<DIMENSION; i++){
         for (int j=0; j<DIMENSION; j++){
             result[i] += matrix_1[i][j] * matrix_2[j];
         }
     }
-    return result;
 }
 
 //1x3 * 3x1 -> 1x1
@@ -60,49 +73,59 @@ double matrix_multiply_1x3_3x1(double matrix_1[DIMENSION], double matrix_2[DIMEN
 }
 
 //3x1 * 1x1 -> 3x1
-double* matrix_scalar_multiply_3x1(double scalar, double matrix_1[DIMENSION]){
-    double result[DIMENSION] = {0,0,0};
+void matrix_scalar_multiply_3x1(double scalar, double matrix_1[DIMENSION], double result[DIMENSION]){
+    result[0] = 0;
+    result[1] = 0;
+    result[2] = 0;
     for (int i=0; i<DIMENSION; i++){
         result[i] = matrix_1[i] * scalar;
     }
-    return result;
 }
 
 //3x1 - 3x1 -> 3x1
-double* matrix_subtraction(double matrix_1[DIMENSION], double matrix_2[DIMENSION]){
-    double result[DIMENSION] = {0,0,0};
+void matrix_subtraction(double matrix_1[DIMENSION], double matrix_2[DIMENSION], double result[DIMENSION]){
+    result[0] = 0;
+    result[1] = 0;
+    result[2] = 0;
     for (int i=0; i<DIMENSION; i++){
         result[i] = matrix_1[i] - matrix_2[i];
     }
-    return result;
 }
 
 //3x1 + 3x1 -> 3x1
-double* matrix_addition(double matrix_1[DIMENSION], double matrix_2[DIMENSION]){
-    double result[DIMENSION] = {0,0,0};
+void matrix_addition(double matrix_1[DIMENSION], double matrix_2[DIMENSION], double result[DIMENSION]){
+    result[0] = 0;
+    result[1] = 0;
+    result[2] = 0;
     for (int i=0; i<DIMENSION; i++){
         result[i] = matrix_1[i] + matrix_2[i];
     }
-    return result;
 }
 
 //1x1
 double create_K(double x, double y, double z){
     double K = pow(x,(double)2) + pow(y,(double)2) + pow(z,(double)2);
+    return K;
 }
 
 //3x3
-double* create_A(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4){
-    double A[DIMENSION][DIMENSION] = {{x2-x1, x3-x1, x4-x1},
-                                    {y2-y1, y3-y1, y4-y1},
-                                    {z2-z1, z3-z1, z4-z1}};
-    return A;
+void create_A(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4, double A[][DIMENSION]){
+    A[0][0] = x2-x1;
+    A[0][1] = x3-x1;
+    A[0][2] = x4-x1;
+    A[1][0] = y2-y1;
+    A[1][1] = y3-y1;
+    A[1][2] = y4-y1;
+    A[2][0] = z2-z1;
+    A[2][1] = z3-z1;
+    A[2][2] = z4-z1;
 }
 
 //3x1
-double* create_b(double K1, double K2, double K3, double K4){
-    double b[DIMENSION] = {(K2 - K1), (K3 - K1), (K4 - K1)};
-    return b;
+void create_b(double K1, double K2, double K3, double K4, double b[DIMENSION]){
+    b[0] = K2 - K1;
+    b[1] = K3 - K1;
+    b[2] = K4 - K1;
 }
 
 double create_ri_1(double ti, double t1){
@@ -110,30 +133,34 @@ double create_ri_1(double ti, double t1){
 }
 
 //3x1
-double* create_d(double r2_1, double r3_1, double r4_1){
-    double d[DIMENSION] = {r2_1, r3_1, r4_1};
-    return d;
+void create_d(double r2_1, double r3_1, double r4_1, double d[DIMENSION]){
+    d[0] = r2_1;
+    d[1] = r3_1;
+    d[2] = r4_1;
 }
 
 //3x1
-double* create_e(double r2_1, double r3_1, double r4_1){
-    double e[DIMENSION] = {pow(r2_1, (double)2), pow(r3_1, (double)2), pow(r4_1, (double)2)};
-    return e;
+void create_e(double r2_1, double r3_1, double r4_1, double e[DIMENSION]){
+    e[0] = pow(r2_1, (double)2);
+    e[1] = pow(r3_1, (double)2);
+    e[2] = pow(r4_1, (double)2);
 }
 
 // ar1^2 + br1 + c = 0
-double* solve_w(double* A, double* b, double* e){
-    double* inverted_A = matrix_invert(A);
-    double* diff_b_e = matrix_subtraction(b,e);
-    double* half_diff_b_e = matrix_scalar_multiply_3x1(0.5, diff_b_e);
-    double* result = matrix_multiply_3x3_3x1(inverted_A, half_diff_b_e);
-    return result;
+void solve_w(double A[][DIMENSION], double b[DIMENSION], double e[DIMENSION], double result[DIMENSION]){
+    double inverted_A[DIMENSION][DIMENSION];
+    matrix_invert(A, inverted_A);
+    double diff_b_e[DIMENSION];
+    matrix_subtraction(b,e, diff_b_e);
+    double half_diff_b_e[DIMENSION];
+    matrix_scalar_multiply_3x1(0.5, diff_b_e, half_diff_b_e);
+    matrix_multiply_3x3_3x1(inverted_A, half_diff_b_e, result);
 }
 
-double* solve_f(double* A, double* d){
-    double* inverted_A = matrix_invert(A);
-    double* result = matrix_multiply_3x3_3x1(inverted_A, d);
-    return result;
+void solve_f(double A[][DIMENSION], double d[DIMENSION], double result[DIMENSION]){
+    double inverted_A[DIMENSION][DIMENSION];
+    matrix_invert(A, inverted_A);
+    matrix_multiply_3x3_3x1(inverted_A, d, result);
 }
 
 double solve_alpha(double* f){
@@ -165,15 +192,20 @@ double solve_r1(double alpha, double beta, double sigma){
 }
 
 // -> 3x1
-double* solve_points(double* A, double* b, double* d, double* e, double r1){
-    double* invert_A = matrix_invert(A);
-    double* r1_d = matrix_scalar_multiply_3x1(r1, d);
-    double* diff_b_e = matrix_subtraction(e,b);
-    double* half_b_e = matrix_scalar_multiply_3x1(0.5, diff_b_e);
-    double* add_b_d_e = matrix_addition(half_b_e, r1_d);
-    double* mult_a_b_d_e = matrix_multiply_3x3_3x1(invert_A, add_b_d_e);
-    double* result = matrix_scalar_multiply_3x1(-1, mult_a_b_d_e);
-    return result;
+void solve_points(double A[][DIMENSION], double b[DIMENSION], double d[DIMENSION], double e[DIMENSION], double r1, double result[DIMENSION]){
+    double invert_A [DIMENSION][DIMENSION];
+    matrix_invert(A, invert_A);
+    double r1_d[DIMENSION];
+    matrix_scalar_multiply_3x1(r1, d, r1_d);
+    double diff_b_e[DIMENSION];
+    matrix_subtraction(e, b, diff_b_e);
+    double half_b_e[DIMENSION]; 
+    matrix_scalar_multiply_3x1(0.5, diff_b_e, half_b_e);
+    double add_b_d_e[DIMENSION];
+    matrix_addition(half_b_e, r1_d, add_b_d_e);
+    double  mult_a_b_d_e[DIMENSION];
+    matrix_multiply_3x3_3x1(invert_A, add_b_d_e, mult_a_b_d_e);
+    matrix_scalar_multiply_3x1(-1, mult_a_b_d_e, result);
 }
 
 int main(){
@@ -207,13 +239,19 @@ int main(){
     double K3 = create_K(x3, y3, z3);
     double K4 = create_K(x4, y4, z4);
 
-    double* A = create_A(x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4);
-    double* b = create_b(K1, K2, K3, K4);
-    double* d = create_d(r2_1, r3_1, r4_1);
-    double* e = create_e(r2_1, r3_1, r4_1);
+    double A[DIMENSION][DIMENSION];
+    create_A(x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4, A);
+    double b[DIMENSION];
+    create_b(K1, K2, K3, K4, b);
+    double d[DIMENSION];
+    create_d(r2_1, r3_1, r4_1, d);
+    double e[DIMENSION];
+    create_e(r2_1, r3_1, r4_1, e);
 
-    double* w = solve_w(A, b, e);
-    double* f = solve_f(A, d);
+    double w[DIMENSION];
+    solve_w(A, b, e, w);
+    double f[DIMENSION];
+    solve_f(A, d, f);
 
     double alpha = solve_alpha(f);
     double beta = solve_beta(f, w);
@@ -221,5 +259,6 @@ int main(){
 
     double r1 = solve_r1(alpha, beta, sigma);
 
-    double* result = solve_points(A, b, d, e, r1);
+    double result[DIMENSION];
+    solve_points(A, b, d, e, r1, result);
 }
