c6db2ccdee50014019bac34dd4fbf9d233568a9d
phann21 <prh5154@psu.edu>
2025-03-30 01:18:30 -0400

    Made arguments pointers

--- Localization.c
+++ Localization.c
@@ -8,7 +8,7 @@
 //BEGIN MATRIX METHODS
 
 // 3x3 -> 1x1
-double matrix_determinant_3x3(double input_matrix[][DIMENSION]){
+double matrix_determinant_3x3(double (*input_matrix)[DIMENSION]){
     double first_line = input_matrix[0][0] * ((input_matrix[1][1] * input_matrix[2][2]) - (input_matrix[1][2] * input_matrix[2][1])); 
     double second_line = input_matrix[0][1] * ((input_matrix[1][0] * input_matrix[2][2]) - (input_matrix[1][2] * input_matrix[2][0]));
     double third_line = input_matrix[0][2] * ((input_matrix[1][0] * input_matrix[2][1]) - (input_matrix[1][1] * input_matrix[2][0]));
@@ -17,7 +17,7 @@ double matrix_determinant_3x3(double input_matrix[][DIMENSION]){
 }
 
 // 3x3 -> 3x3
-void matrix_transpose_3x3(double input_matrix[][DIMENSION], double output_matrix[][DIMENSION]){
+void matrix_transpose_3x3(double (*input_matrix)[DIMENSION], double (*output_matrix)[DIMENSION]){
     for (int i=0; i<DIMENSION; i++){
         for (int j=0; j<DIMENSION; j++){
             output_matrix[i][j] = input_matrix[j][i];
@@ -26,7 +26,7 @@ void matrix_transpose_3x3(double input_matrix[][DIMENSION], double output_matrix
 }
 
 // 3x3 -> 3x3
-void matrix_invert(double input_matrix[][DIMENSION], double output_matrix[][DIMENSION]){
+void matrix_invert(double (*input_matrix)[DIMENSION], double (*output_matrix)[DIMENSION]){
     double adjoint_matrix[DIMENSION][DIMENSION];
     adjoint_matrix[0][0] = input_matrix[1][1] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][1];
     adjoint_matrix[0][1] = (-1) * (input_matrix[1][0] * input_matrix[2][2] - input_matrix[1][2] * input_matrix[2][0]);
@@ -55,7 +55,7 @@ void matrix_invert(double input_matrix[][DIMENSION], double output_matrix[][DIME
 }
 
 // 3x3 * 3x1 -> 3x1
-void matrix_multiply_3x3_3x1(double matrix_1[DIMENSION][DIMENSION], double matrix_2[DIMENSION], double result[DIMENSION]){
+void matrix_multiply_3x3_3x1(double (*matrix_1)[DIMENSION], double* matrix_2, double* result){
     result[0] = 0;
     result[1] = 0;
     result[2] = 0;
@@ -67,7 +67,7 @@ void matrix_multiply_3x3_3x1(double matrix_1[DIMENSION][DIMENSION], double matri
 }
 
 // 1x3 * 3x1 -> 1x1
-double matrix_multiply_1x3_3x1(double matrix_1[DIMENSION], double matrix_2[DIMENSION]){
+double matrix_multiply_1x3_3x1(double* matrix_1, double* matrix_2){
     double sum = 0;
     for (int i=0; i<DIMENSION; i++){
         sum += matrix_1[i] * matrix_2[i];
@@ -76,14 +76,14 @@ double matrix_multiply_1x3_3x1(double matrix_1[DIMENSION], double matrix_2[DIMEN
 }
 
 // 3x1 * 1x1 -> 3x1
-void matrix_scalar_multiply_3x1(double scalar, double matrix_1[DIMENSION], double result[DIMENSION]){
+void matrix_scalar_multiply_3x1(double scalar, double* matrix_1, double* result){
     for (int i=0; i<DIMENSION; i++){
         result[i] = matrix_1[i] * scalar;
     }
 }
 
 // 3x1 - 3x1 -> 3x1
-void matrix_subtraction(double matrix_1[DIMENSION], double matrix_2[DIMENSION], double result[DIMENSION]){
+void matrix_subtraction(double* matrix_1, double* matrix_2, double* result){
     for (int i=0; i<DIMENSION; i++){
         result[i] = matrix_1[i] - matrix_2[i];
     }
@@ -108,7 +108,7 @@ double create_Ki(double x, double y, double z){
 // [[x2-x1; y2-y1; z2-z1;],
 // [x3-x1; y3-y1; z3-z1;],
 // [x4-x1; y4-y1; z4-z1;]]
-void create_A(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4, double A[][DIMENSION]){
+void create_A(double x1, double x2, double x3, double x4, double y1, double y2, double y3, double y4, double z1, double z2, double z3, double z4, double (*A)[DIMENSION]){
     A[0][0] = x2-x1;
     A[0][1] = y2-y1;
     A[0][2] = z2-z1;
@@ -129,7 +129,7 @@ void create_A(double x1, double x2, double x3, double x4, double y1, double y2, 
 }
 
 // 3x1
-void create_b(double r2_1, double r3_1, double r4_1, double b[DIMENSION]){
+void create_b(double r2_1, double r3_1, double r4_1, double* b){
     b[0] = r2_1;
     b[1] = r3_1;
     b[2] = r4_1;
@@ -139,7 +139,7 @@ void create_b(double r2_1, double r3_1, double r4_1, double b[DIMENSION]){
 }
 
 // 3x1
-void create_d(double r2_1, double r3_1, double r4_1, double K1, double K2, double K3, double K4, double d[DIMENSION]){
+void create_d(double r2_1, double r3_1, double r4_1, double K1, double K2, double K3, double K4, double* d){
     d[0] = pow(r2_1, 2) - K2 + K1;
     d[1] = pow(r3_1, 2) - K3 + K1;
     d[2] = pow(r3_1, 2) - K4 + K1;
@@ -150,10 +150,10 @@ void create_d(double r2_1, double r3_1, double r4_1, double K1, double K2, doubl
 
 // END INITIALIZING VARIABLES
 
-// BEGIN HELPER VARIABLES
+// BEGIN INTERMEDIATE VARIABLES
 
 // f = -(A^-1) * b
-void solve_f(double invert_A[][DIMENSION], double b[DIMENSION], double result[DIMENSION]){
+void solve_f(double (*invert_A)[DIMENSION], double* b, double* result){
     // for (int k=0; k<DIMENSION; k++){
     //     for (int l=0; l<DIMENSION; l++){
     //         printf("inverted_A %lf ", inverted_A[k][l]);
@@ -176,7 +176,7 @@ void solve_f(double invert_A[][DIMENSION], double b[DIMENSION], double result[DI
 }
 
 // g = 0.5(-(A^-1) * d)
-void solve_g(double invert_A[][DIMENSION], double d[DIMENSION], double result[DIMENSION]){
+void solve_g(double (*invert_A)[DIMENSION], double* d, double* result){
     double A_invert_d[DIMENSION];
     matrix_multiply_3x3_3x1(invert_A, d, A_invert_d);
     matrix_scalar_multiply_3x1(0.5, A_invert_d, result);
@@ -203,7 +203,7 @@ double solve_gamma(double* g){
     return result;
 }
 
-// END HELPER VARIABLES
+// END INTERMEDIATE VARIABLES
 
 // quadratic formula -beta +/- sqrt(b)
 double solve_r1(double alpha, double beta, double gamma){
@@ -222,7 +222,7 @@ double solve_r1(double alpha, double beta, double gamma){
 }
 
 //returns -> 3x1
-void solve_points(double invert_A[][DIMENSION], double b[DIMENSION], double d[DIMENSION], double r1, double result[DIMENSION]){
+void solve_points(double (*invert_A)[DIMENSION], double* b, double* d, double r1, double* result){
     double r1_b[DIMENSION];
     matrix_scalar_multiply_3x1(r1, b, r1_b);
     double A_invert_b_r1[DIMENSION];
