60e6a11d5aaaaa80af41acb89093093b89972010
phann21 <prh5154@psu.edu>
2025-04-01 12:29:07 -0400

    Final?

--- Localization.c
+++ Localization.c
@@ -8,6 +8,7 @@
 
 #define DIMENSION 3
 #define C_speed 1481*1000 //speed of sound in mm/s
+
 /* *********************************************************************************************************** */
 // *****END INCLUDES AND DEFINES***************************************************************************** //
 /* ********************************************************************************************************* */
@@ -15,7 +16,6 @@
 /* ******************************************************************************************************* */
 // *****BEGIN MATRIX METHODS***************************************************************************** //
 /* ***************************************************************************************************** */
-
 
 // 3x3 -> 1x1
 double matrix_determinant_3x3(double (*input_matrix)[DIMENSION]){
@@ -52,19 +52,14 @@ int matrix_invert(double (*input_matrix)[DIMENSION], double (*output_matrix)[DIM
     if (determinant == 0){
         return 1;
     }
+
     for (int i = 0; i < DIMENSION; i++){
         for (int j = 0; j < DIMENSION; j++){
             adjoint_matrix[i][j] = adjoint_matrix[i][j] / determinant;
         }
     }
-    // printf("determinant %lf\n", determinant);
+
     matrix_transpose_3x3(adjoint_matrix, output_matrix);
-    // for (int k=0; k<DIMENSION; k++){
-    //     for (int l=0; l<DIMENSION; l++){
-    //         printf("Inverted A %lf ", output_matrix[k][l]);
-    //     }
-    //     printf("\n");
-    // }
     return 0;
 }
 
@@ -102,6 +97,7 @@ void matrix_subtraction(double* matrix_1, double* matrix_2, double* result){
         result[i] = matrix_1[i] - matrix_2[i];
     }
 }
+
 /* ***************************************************************************************************** */
 // *****END MATRIX METHODS***************************************************************************** //
 /* *************************************************************************************************** */
@@ -111,8 +107,8 @@ void matrix_subtraction(double* matrix_1, double* matrix_2, double* result){
 /* ************************************************************************************************************* */
 
 // 1x1 ri_1 = C_speed(t_i - t1)
-double create_ri_1(double t_i, double t1){
-    return C_speed*(t_i - t1);
+double create_ri_1(double t_i, double t_1){
+    return C_speed*(t_i - t_1);
 }
 
 // 1x1 K_i = x_i^2 + y_i^2 + z_i^2
@@ -137,12 +133,6 @@ void create_A(double x1, double x2, double x3, double x4, double y1, double y2, 
     A[2][0] = x4-x1;
     A[2][1] = y4-y1;
     A[2][2] = z4-z1;
-    // for (int k=0; k<DIMENSION; k++){
-    //     for (int l=0; l<DIMENSION; l++){
-    //         printf("A %lf ", A[k][l]);
-    //     }
-    //     printf("\n");
-    // }
 }
 
 // 3x1
@@ -150,9 +140,6 @@ void create_b(double r2_1, double r3_1, double r4_1, double* b){
     b[0] = r2_1;
     b[1] = r3_1;
     b[2] = r4_1;
-    // for (int k=0; k<DIMENSION; k++){
-    //     printf("B %lf\n", b[k]);
-    // }
 }
 
 // 3x1
@@ -160,9 +147,6 @@ void create_d(double r2_1, double r3_1, double r4_1, double K1, double K2, doubl
     d[0] = pow(r2_1, 2) - K2 + K1;
     d[1] = pow(r3_1, 2) - K3 + K1;
     d[2] = pow(r4_1, 2) - K4 + K1;
-    // for (int k=0; k<DIMENSION; k++){
-    //     printf("D %lf\n", d[k]);
-    // }
 }
 
 /* ************************************************************************************************************* */
@@ -175,25 +159,9 @@ void create_d(double r2_1, double r3_1, double r4_1, double K1, double K2, doubl
 
 // f = -(A^-1) * b
 void solve_f(double (*invert_A)[DIMENSION], double* b, double* result){
-    // for (int k=0; k<DIMENSION; k++){
-    //     for (int l=0; l<DIMENSION; l++){
-    //         printf("inverted_A %lf ", inverted_A[k][l]);
-    //     }
-    //     printf("\n");
-    // }
-
-    // for (int k=0; k<DIMENSION; k++){
-    //     printf("b %lf\n", b[k]);
-    // }
     double A_invert_b[DIMENSION];
     matrix_multiply_3x3_3x1(invert_A, b, A_invert_b);
-    // for (int k=0; k<DIMENSION; k++){
-    //     printf("A_invert_b %lf\n", A_invert_b[k]);
-    // }
     matrix_scalar_multiply_3x1(-1, A_invert_b, result);
-    // for (int k=0; k<DIMENSION; k++){
-    //     printf("f %lf\n", result[k]);
-    // }
 }
 
 // g = 0.5((A^-1) * d)
@@ -201,9 +169,6 @@ void solve_g(double (*invert_A)[DIMENSION], double* d, double* result){
     double A_invert_d[DIMENSION];
     matrix_multiply_3x3_3x1(invert_A, d, A_invert_d);
     matrix_scalar_multiply_3x1(0.5, A_invert_d, result);
-    // for (int k=0; k<DIMENSION; k++){
-    //     printf("g %lf\n", result[k]);
-    // }
 }
 
 // alpha = fTf - 1
@@ -242,24 +207,16 @@ double solve_r1(double alpha, double beta, double gamma){
     double result_2 = (-1 * beta - sqrt(pow(beta, 2) - 4 * alpha * gamma)) / (2 * alpha);
     if (result_1 > 0 && result_2 > 0){
         if (result_1 > result_2){
-            // printf("result_2 chosen r2: %lf\n", result_2);
-            // printf("result_2 chosen r1: %lf\n", result_1);
             return result_2;
         }
         else{
-            // printf("result_1 chosen r1: %lf\n", result_1);
-            // printf("result_1 chosen r2: %lf\n", result_2);
             return result_1;
         }
     }
     if (result_1 < 0){
-        // printf("result_2 chosen r2: %lf\n", result_2);
-        // printf("result_2 chosen r1: %lf\n", result_1);
         return result_2;
     }
     else{
-        // printf("result_1 chosen r1: %lf\n", result_1);
-        // printf("result_1 chosen r2: %lf\n", result_2);
         return result_1;
     }
 }
@@ -284,57 +241,30 @@ void solve_points(double (*invert_A)[DIMENSION], double* b, double* d, double r1
 /* ************************************************************************************************** */
 
 int main(){
-    clock_t begin = clock();
     /******BEGIN USER INPUTS******/
     // inputs in seconds
-    // double t1 = 1.350438892640108e-05;
-    // double t2 = 9.549044985638725e-06;
-    // double t3 = 9.548939945566158e-06;
-    // double t4 = 9.548939945566158e-06;
-    // double t1 = 5.4855087134611484e-06;
-    // double t2 = 1.3037280047565443e-05;
-    // double t3 = 1.8108311020723282e-05;
-    // double t4 = 6.2617275459120215e-06;
-    double t1 = 5.4855087134611484e-06;
-    double t2 = 1.3037280047565443e-05;
-    double t3 = 1.8108311020723282e-05;
-    double t4 = 1.181151632986894e-05;
-    
+    double t1 = 1.350438892640108e-05;
+    double t2 = 1.816851097513353e-05;
+    double t3 = 1.816851097513353e-05;
+    double t4 = 1.816829716012655e-05;
 
-    
-
-    // double x1 = 0;
-    // double y1 = 0;
-    // double z1 = 0;
-
-    // double x2 = 0;
-    // double y2 = 10;
-    // double z2 = -10;
-
-    // double x3 = -8.66;
-    // double y3 = -5;
-    // double z3 = -10;
-
-    // double x4 = 8.66;
-    // double y4 = -5;
-    // double z4 = -10;
     double x1 = 0;
     double y1 = 0;
     double z1 = 0;
 
     double x2 = -10;
     double y2 = 17.321;
-    double z2 = -10;
+    double z2 = -2;
 
     double x3 = -10;
     double y3 = -17.321;
-    double z3 = -10;
+    double z3 = -2;
 
     double x4 = 20;
     double y4 = 0;
-    double z4 = -10;
+    double z4 = -2;
     // *****END USER INPUTS****** //
-
+    // clock_t begin = clock(); *****Uncomment to perform time analysis*****
     double r2_1 = create_ri_1(t2, t1);
     double r3_1 = create_ri_1(t3, t1);
     double r4_1 = create_ri_1(t4, t1);
@@ -370,11 +300,11 @@ int main(){
     double result[DIMENSION];
     solve_points(invert_A, b, d, r1, result);
 
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    // clock_t end = clock(); *****Uncomment to perform time analysis*****
+    // double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; *****Uncomment to perform time analysis*****
 
     for (int i=0; i<DIMENSION; i++){
         printf("Value %lf\n", result[i]);
     }
-    printf("Time spent: %lfs\n", time_spent);
+    // printf("Time spent: %lfs\n", time_spent);
 }
